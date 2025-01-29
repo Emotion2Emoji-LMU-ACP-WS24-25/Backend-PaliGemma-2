b52df5b85cbe11b2242efedd09e5080a3608d82c
Leon Lantz <leon.lantz14@gmail.com>
2025-01-29 17:24:22 +0100

    Optimize image analysis
    - add FaceScanPaliGemma_Emotion+
    - add backImage analysis

--- app.py
+++ app.py
@@ -8,14 +8,22 @@ import time
 from PIL import Image
 from pymongo import MongoClient
 
+# Define model paths and MongoDB URI
 MODEL_PATH = "/app/models/paligemma2-3b-ft-docci-448"
+EMOTION_MODEL_PATH = "/app/models/FaceScanPaliGemma_Emotion"
+MODEL_PATH2 = "/app/models/paligemma-3b-pt-224"
 MONGO_URI = "mongodb://mongodb:27017"
 DB_NAME = "imageUploadDB"
 
-model = PaliGemmaForConditionalGeneration.from_pretrained(MODEL_PATH, torch_dtype=torch.bfloat16, local_files_only=True, device_map="auto").eval()
+# Load models and processors
+model = PaliGemmaForConditionalGeneration.from_pretrained(MODEL_PATH, torch_dtype=torch.bfloat16, local_files_only=True)
 processor = PaliGemmaProcessor.from_pretrained(MODEL_PATH)
 
-# Initialize MongoDB client
+emotion_analysis_model = PaliGemmaForConditionalGeneration.from_pretrained(EMOTION_MODEL_PATH, torch_dtype=torch.bfloat16, local_files_only=True, device_map="auto")
+emotion_analysis_processor = PaliGemmaProcessor.from_pretrained(MODEL_PATH2, local_files_only=True)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Initialize MongoDB client and database
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
@@ -28,8 +36,8 @@ except Exception as e:
 
 def get_pending_jobs():
     """
-    This function retrieves all entries with the status 'prompt_created' from all collections,
-    sorted by creation date (oldest first).
+    This function retrieves all entries with the status 'uploaded' from all collections,
+    sorted by upload date (oldest first).
     """
     pending_jobs = []
 
@@ -37,7 +45,7 @@ def get_pending_jobs():
     for collection_name in db.list_collection_names():
         collection = db[collection_name]
 
-        # Find all entries with the status 'prompt_created' and sort by creation date
+        # Find all entries with the status 'uploaded' and sort by upload date
         jobs = collection.find({"status": "uploaded"}).sort("uploadDate", 1)  # 1 for ascending order
         # Add the found jobs to the pending jobs list
         for job in jobs:
@@ -74,6 +82,7 @@ def analyze_images():
         try:
             # Extract prompt
             frontImagePath = oldest_job['entry']["frontImagePath"]
+            backImagePath = oldest_job['entry']["backImagePath"]
             print("Analyzing image:", frontImagePath)
 
             # Update the status in the database to "generating"
@@ -83,37 +92,55 @@ def analyze_images():
             )
 
             print("Loading")
-            image = Image.open(frontImagePath)
-            prompts = [ '<image><bos> answer en What is the gender of the person in the image? Male or female?\n',
-                        '<image><bos> answer en What is the hair color, hair length and hairstyle of the person in the image?\n',
-                        '<image><bos> answer en What is the eye color of the person in the image?\n',
-                        '<image><bos> answer en What emotion is the person in the image strongly expressing?\n',
-                        '<image><bos> answer en Summarize in maximum 6 words the clothing and its color.\n',
-                        '<image><bos> answer en Summarize in 5 words: the background of the image?\n',
-                        '<image><bos> answer en Is the person wearing glasses?\n',
+            front_image = Image.open(frontImagePath)
+            back_image = Image.open(backImagePath)
+
+            prompts = [ '<image><bos> describe en\n',
+                        '<image><bos> caption en\n',
             ]
 
-            images = [image] * len(prompts)  # Assuming the same image for all questions
+            images = [front_image, back_image]
 
-            model_inputs = processor(text=prompts, images=images, padding=True, return_tensors="pt").to(torch.bfloat16).to(model.device)
+            model.to(device)
+            model_inputs = processor(text=prompts, images=images, padding=True, return_tensors="pt").to(torch.bfloat16).to(device)
             input_len = model_inputs["input_ids"].shape[-1]
 
             with torch.inference_mode():
-                generation = model.generate(**model_inputs, max_new_tokens=30, do_sample=False)
+                generation = model.generate(**model_inputs, max_new_tokens=150, do_sample=False)
                 generated_texts = generation[:, input_len:]
                 decoded_texts = [processor.decode(text, skip_special_tokens=True) for text in generated_texts]
 
-            text_prompt = f'A {decoded_texts[0]} with {decoded_texts[1]} hair, {decoded_texts[2]} eyes, strongly expressing {decoded_texts[3]} emotion, wearing {decoded_texts[4]}, in a setting with {decoded_texts[5]}, , icon emoji'
-            print(text_prompt)
+            front_image_description = decoded_texts[0]
+            back_image_description = decoded_texts[1]
+
+            emotion_analysis_model.to(device)
+            input_text = "Answer en What is the emotion of the main person in the image? choose from: ‘neutral’, \t ‘happy’, \t ‘sad’ \t, ‘surprise’ \t ‘fear’ \t, ‘disgust’,\t ‘angry’ \n"
+            inputs = emotion_analysis_processor(text=input_text, images=front_image, padding="longest", do_convert_rgb=True, return_tensors="pt").to(device)
+            inputs = inputs.to(dtype=emotion_analysis_model.dtype)
+
+            with torch.no_grad():
+                output = emotion_analysis_model.generate(**inputs, max_length=500)
+                result=emotion_analysis_processor.decode(output[0], skip_special_tokens=True)[len(input_text):].strip()
+
+            emotion = result
+
             collection.update_one(
                     {"_id": oldest_job['entry']['_id']}, 
-                    {"$set": {"prompt": text_prompt}}
+                    {"$set": {"frontImageDescription": front_image_description}}
+            )
+            collection.update_one(
+                    {"_id": oldest_job['entry']['_id']}, 
+                    {"$set": {"backImageDescription": back_image_description}}
+            )
+            collection.update_one(
+                    {"_id": oldest_job['entry']['_id']}, 
+                    {"$set": {"emotion": emotion}}
             )
             
             # Update the status in the database to "prompt_created"
             collection.update_one(
                 {"_id": oldest_job['entry']['_id']}, 
-                {"$set": {"status": "prompt_created"}}
+                {"$set": {"status": "analyzed"}}
             )
 
         except Exception as e:
